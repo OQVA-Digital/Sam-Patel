cc1cebb181c925b69011740e8f767a9093eaea3c
Dex REAU <dev@oqva.digital>
2023-11-22 09:20:18 -0300

    up

---
+++ src/consts.ts
@@ -0,0 +1,21 @@
+
+// META
+
+export const SITE_TITLE = "Lorem";
+
+export const SITE_DESCRIPTION = "Ipsum";
+
+export const SITE_KEYWORDS = "Dolor"
+    
+
+// SOCIAL SHARING IMAGE
+
+export const OG_IMAGE = '/og/social-image.jpg'
+
+
+// TEL
+
+export const TEL = "+44 (0) 78520 72648"
+export const TEL_HREF = ""
+export const TEL_INTERNATIONAL = ""
+export const EMAIL = "Sam@SamitPatel.org"
